e7e46cd116585dd3b124560c3ce2e0642f9a4a66
dldbsgh00005 <dldbsgh00005@gmail.com>
2025-02-10 08:40:32 +0900

    update : remove unuse components

--- lee-pjt-01-frontend/src/App.jsx
+++ lee-pjt-01-frontend/src/App.jsx
@@ -1,13 +1,11 @@
-// import { useState } from 'react'
+import Home from './Components/Home'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
+          <Home />
       </div>
-    </>
   )
 }
 
